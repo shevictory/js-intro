d620257d616883733ffc70b7c87b0828cd11a9f7
Viktoriia <shevictory@gmail.com>
2021-05-23 14:00:34 +0300

    next

--- js/classSyntax.js
+++ js/classSyntax.js
@@ -8,3 +8,35 @@ class Animal {
 }
 const a = new Animal('Milka');
 a.logName();
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  set age(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError();
+    }
+    if (value < 0 || value > 150) {
+      throw new RangeError();
+    }
+    this._age = value;
+  }
+  get age() {
+    return this._age;
+  }
+  static isUser(obj) {
+    return obj instanceof User;
+  }
+  static isAdult(obj) {
+    if (User.isUser(obj)) {
+      throw new TypeError();
+    }
+    return obj.isAdult();
+  }
+}
+// debugger;
+const b = new User('Leo', 5);
+console.log('b.age :>> ', b.age);
+console.log('User.isUser(b) :>> ', User.isUser(b));
